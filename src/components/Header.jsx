1237c7d1302531a703b9d7076374da830c02ff2e
Pulkit2007 <bansalpulkit2007@outlook.com>
2026-09-11 21:08:16 +0530

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+
+export default function Header(){
+    return(
+        <header>
+            <img  className= "icon" src="/src/assets/chef-claude-icon.png" />
+            <h1>Chef Claude</h1>
+        </header>
+    )
+}
